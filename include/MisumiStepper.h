48d811a75f21c41ae4b5f62d235840ceb0517e3f
Koki Kokubo <cotechworks63@gmail.com>
2026-03-10 23:29:45 +0900

    初回コミット

---
+++ include/MisumiStepper.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <Arduino.h>
+
+enum MovingMode { CONTINUOUS, STEP };
+
+class MisumiStepper {
+ public:
+  /**
+   * コンストラクタ
+   * @param enaPin ENAピン
+   * @param dirPin DIRピン
+   * @param pulPin PULピン
+   * @param mode 動作モード (CONTINUOUS or STEP)
+   * @param handler_period_us ハンドラの呼び出し周期(us)
+   * @param pulse_count 1回転あたりのパルス数
+   * @param speed 速度 (rpm)
+   * @param target_speed 目標速度 (rpm)
+   */
+  MisumiStepper(int enaPin, int dirPin, int pulPin, MovingMode mode,
+                uint32_t period_us, uint32_t pulse_num, uint32_t speed,
+                float acceleration = 0.0);
+
+  /**
+   * 動作モードの設定
+   * @param m 動作モード (CONTINUOUS or STEP)
+   */
+  void setMovingMode(MovingMode m);
+  /**
+   * 速度の設定
+   * @param speed 速度 (rpm)
+   */
+  void setSpeed(float speed);
+  void setSpeed(int speed);
+  void setSpeed(double speed);
+  /**
+   * 動作開始
+   */
+  void start();
+  /**
+   * 動作停止 (停止時の位置を保持)
+   */
+  void hold();
+  /**
+   * 動作停止
+   */
+  void stop();
+  /**
+   * ステッパーモーターのハンドラ (タイマー割り込み等で定期的に呼び出す)
+   */
+  void StepperHandler();
+
+ private:
+  bool _state = false;            // true: 動作中, false: 停止中
+  MovingMode _mode = CONTINUOUS;  // 動作モード
+  int _enaPin;                    // ENAピン
+  int _dirPin;                    // DIRピン
+  int _pulPin;                    // PULピン
+  uint32_t _handler_period_us;    // ハンドラの呼び出し周期(us)
+  float _speed;                   // 速度(rpm)
+  float _target_speed;            // 目標速度(rpm)
+  float _acceleration;            // 加速度(rpm/_handler_period_us)
+  uint32_t _time_count = 0;       // ハンドラ呼び出し間隔の積算値(us)
+  uint32_t _pulse_count;          // 1回転あたりのパルス数
+};
---
+++ src/MisumiStepper.cpp
@@ -0,0 +1,102 @@
+#include "MisumiStepper.h"
+
+MisumiStepper::MisumiStepper(int enaPin, int dirPin, int pulPin,
+                             MovingMode mode, uint32_t handler_period_us,
+                             uint32_t pulse_count, uint32_t speed,
+                             float acceleration) {
+  _enaPin = enaPin;
+  _dirPin = dirPin;
+  _pulPin = pulPin;
+  _mode = mode;
+  _handler_period_us = handler_period_us;
+  _speed = speed;
+  _target_speed = speed;
+  _pulse_count = pulse_count;
+  _acceleration = acceleration;
+
+  pinMode(_enaPin, OUTPUT);
+  pinMode(_dirPin, OUTPUT);
+  pinMode(_pulPin, OUTPUT);
+  digitalWrite(_enaPin, HIGH);
+  digitalWrite(_dirPin, LOW);
+  digitalWrite(_pulPin, LOW);
+}
+
+void MisumiStepper::setMovingMode(MovingMode m) { _mode = m; }
+
+void MisumiStepper::setSpeed(float speed) {
+  _target_speed = speed;
+  if (_acceleration == 0.0) {
+    _speed = speed;
+  }
+  delay(10);
+}
+
+void MisumiStepper::setSpeed(double speed) {
+  _speed = static_cast<float>(speed);
+  delay(10);
+}
+
+void MisumiStepper::setSpeed(int speed) {
+  _speed = static_cast<float>(speed);
+  delay(10);
+}
+
+void MisumiStepper::start() {
+  _state = true;
+  digitalWrite(_enaPin, LOW);
+  digitalWrite(_dirPin, HIGH);
+}
+
+void MisumiStepper::hold() {
+  _state = false;
+  digitalWrite(_enaPin, LOW);
+  digitalWrite(_dirPin, HIGH);
+  digitalWrite(_pulPin, LOW);
+}
+
+void MisumiStepper::stop() {
+  _state = false;
+  digitalWrite(_enaPin, HIGH);
+  digitalWrite(_dirPin, LOW);
+  digitalWrite(_pulPin, LOW);
+}
+
+void MisumiStepper::StepperHandler() {
+  if (_state == false) {
+    return;
+  }
+
+  if (_mode == CONTINUOUS) {
+    // 時間の更新
+    _time_count += _handler_period_us;
+
+    // 速度の更新
+    if (_acceleration > 0.0) {
+      if (_speed < _target_speed) {
+        _speed += _acceleration;
+        if (_speed > _target_speed) _speed = _target_speed;
+      } else if (_speed > _target_speed) {
+        _speed -= _acceleration;
+        if (_speed < _target_speed) _speed = _target_speed;
+      }
+    }
+
+    // 回転方向の設定
+    if (_speed >= 0) {
+      digitalWrite(_dirPin, HIGH);
+    } else {
+      digitalWrite(_dirPin, LOW);
+    }
+
+    // パルスの生成間隔の計算
+    uint32_t wait = static_cast<uint32_t>(
+        60000000.0 / (static_cast<float>(_pulse_count) * 2.0 * _speed));
+
+    // パルスの生成
+    if (_time_count >= wait) {
+      _time_count = 0;
+      digitalWrite(_pulPin, 1 - digitalRead(_pulPin));
+    }
+  }
+}
