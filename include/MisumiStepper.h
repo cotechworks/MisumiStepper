9123e6e39cf4949d642041588c99de664ebe71e6
Koki Kokubo <cotechworks63@gmail.com>
2026-04-13 14:37:20 +0900

    .clang-format導入

--- include/MisumiStepper.h
+++ include/MisumiStepper.h
@@ -4,8 +4,9 @@
 
 enum MovingMode { CONTINUOUS, STEP };
 
-class MisumiStepper {
- public:
+class MisumiStepper
+{
+public:
   /**
    * コンストラクタ
    * @param enaPin ENAピン
@@ -17,59 +18,65 @@ class MisumiStepper {
    * @param speed 速度 (rpm)
    * @param target_speed 目標速度 (rpm)
    */
-  MisumiStepper(int enaPin, int dirPin, int pulPin, MovingMode mode,
-                uint32_t period_us, uint32_t pulse_num, uint32_t speed = 60.0,
-                float acceleration = 0.0);
+  MisumiStepper( int enaPin, int dirPin, int pulPin, MovingMode mode, uint32_t period_us, uint32_t pulse_num, uint32_t speed = 60.0,
+                 float acceleration = 0.0 );
 
   /**
    * 動作モードの設定
    * @param m 動作モード (CONTINUOUS or STEP)
    */
-  void setMovingMode(MovingMode m);
+  void setMovingMode( MovingMode m );
+
   /**
    * 速度の設定
    * @param speed 速度 (rpm)
    */
-  void setSpeed(float speed);
-  void setSpeed(int speed);
-  void setSpeed(double speed);
+  void setSpeed( float speed );
+  void setSpeed( int speed );
+  void setSpeed( double speed );
+
   /**
    * 速度の取得
    * @return 速度 (rpm)
    */
   float getSpeed();
+
   /**
    * 状態の取得
    * @return true: 動作中, false: 停止中
    */
   bool getState();
+
   /**
    * 動作開始
    */
   void start();
+
   /**
    * 動作停止 (停止時の位置を保持)
    */
   void hold();
+
   /**
    * 動作停止
    */
   void stop();
+
   /**
    * ステッパーモーターのハンドラ (タイマー割り込み等で定期的に呼び出す)
    */
   void StepperHandler();
 
- private:
-  bool _state = false;            // true: 動作中, false: 停止中
-  MovingMode _mode = CONTINUOUS;  // 動作モード
-  int _enaPin;                    // ENAピン
-  int _dirPin;                    // DIRピン
-  int _pulPin;                    // PULピン
-  uint32_t _handler_period_us;    // ハンドラの呼び出し周期(us)
-  float _speed;                   // 速度(rpm)
-  float _target_speed;            // 目標速度(rpm)
-  float _acceleration;            // 加速度(rpm/_handler_period_us)
-  uint32_t _time_count = 0;       // ハンドラ呼び出し間隔の積算値(us)
-  uint32_t _pulse_count;          // 1回転あたりのパルス数
+private:
+  bool _state = false;           // true: 動作中, false: 停止中
+  MovingMode _mode = CONTINUOUS; // 動作モード
+  int _enaPin;                   // ENAピン
+  int _dirPin;                   // DIRピン
+  int _pulPin;                   // PULピン
+  uint32_t _handler_period_us;   // ハンドラの呼び出し周期(us)
+  float _speed;                  // 速度(rpm)
+  float _target_speed;           // 目標速度(rpm)
+  float _acceleration;           // 加速度(rpm/_handler_period_us)
+  uint32_t _time_count = 0;      // ハンドラ呼び出し間隔の積算値(us)
+  uint32_t _pulse_count;         // 1回転あたりのパルス数
 };
--- src/MisumiStepper.cpp
+++ src/MisumiStepper.cpp
@@ -1,9 +1,8 @@
 #include "MisumiStepper.h"
 
-MisumiStepper::MisumiStepper(int enaPin, int dirPin, int pulPin,
-                             MovingMode mode, uint32_t handler_period_us,
-                             uint32_t pulse_count, uint32_t speed,
-                             float acceleration) {
+MisumiStepper::MisumiStepper( int enaPin, int dirPin, int pulPin, MovingMode mode, uint32_t handler_period_us, uint32_t pulse_count,
+                              uint32_t speed, float acceleration )
+{
   _enaPin = enaPin;
   _dirPin = dirPin;
   _pulPin = pulPin;
@@ -14,96 +13,93 @@ MisumiStepper::MisumiStepper(int enaPin, int dirPin, int pulPin,
   _pulse_count = pulse_count;
   _acceleration = acceleration;
 
-  pinMode(_enaPin, OUTPUT);
-  pinMode(_dirPin, OUTPUT);
-  pinMode(_pulPin, OUTPUT);
-  digitalWrite(_enaPin, HIGH);
-  digitalWrite(_dirPin, LOW);
-  digitalWrite(_pulPin, LOW);
+  pinMode( _enaPin, OUTPUT );
+  pinMode( _dirPin, OUTPUT );
+  pinMode( _pulPin, OUTPUT );
+  digitalWrite( _enaPin, HIGH );
+  digitalWrite( _dirPin, LOW );
+  digitalWrite( _pulPin, LOW );
 }
 
-void MisumiStepper::setMovingMode(MovingMode m) { _mode = m; }
+void MisumiStepper::setMovingMode( MovingMode m ) { _mode = m; }
 
-void MisumiStepper::setSpeed(float speed) {
+void MisumiStepper::setSpeed( float speed )
+{
   _target_speed = speed;
-  if (_acceleration == 0.0) {
+  if( _acceleration == 0.0 ) {
     _speed = speed;
   }
-  delay(10);
+  delay( 10 );
 }
 
-void MisumiStepper::setSpeed(double speed) {
-  setSpeed(static_cast<float>(speed));
-}
+void MisumiStepper::setSpeed( double speed ) { setSpeed( static_cast<float>( speed ) ); }
 
-void MisumiStepper::setSpeed(int speed) {
-  setSpeed(static_cast<float>(speed));
-}
+void MisumiStepper::setSpeed( int speed ) { setSpeed( static_cast<float>( speed ) ); }
 
-float MisumiStepper::getSpeed() {
-  return _speed;
-}
+float MisumiStepper::getSpeed() { return _speed; }
 
-void MisumiStepper::start() {
 bool MisumiStepper::getState() { return _state; }
 
+void MisumiStepper::start()
+{
   _state = true;
-  _speed = 0.0;
-  digitalWrite(_enaPin, LOW);
-  digitalWrite(_dirPin, HIGH);
+  digitalWrite( _enaPin, LOW );
+  digitalWrite( _dirPin, HIGH );
 }
 
-void MisumiStepper::hold() {
+void MisumiStepper::hold()
+{
   _state = false;
   _speed = 0.0;
-  digitalWrite(_enaPin, LOW);
-  digitalWrite(_dirPin, HIGH);
-  digitalWrite(_pulPin, LOW);
+  digitalWrite( _enaPin, LOW );
+  digitalWrite( _dirPin, HIGH );
+  digitalWrite( _pulPin, LOW );
 }
 
-void MisumiStepper::stop() {
+void MisumiStepper::stop()
+{
   _state = false;
   _speed = 0.0;
-  digitalWrite(_enaPin, HIGH);
-  digitalWrite(_dirPin, LOW);
-  digitalWrite(_pulPin, LOW);
+  digitalWrite( _enaPin, HIGH );
+  digitalWrite( _dirPin, LOW );
+  digitalWrite( _pulPin, LOW );
 }
 
-void MisumiStepper::StepperHandler() {
-  if (_state == false) {
+void MisumiStepper::StepperHandler()
+{
+  if( _state == false ) {
     return;
   }
 
-  if (_mode == CONTINUOUS) {
+  if( _mode == CONTINUOUS ) {
     // 時間の更新
     _time_count += _handler_period_us;
 
     // 速度の更新
-    if (_acceleration > 0.0) {
-      if (_speed < _target_speed) {
+    if( _acceleration > 0.0 ) {
+      if( _speed < _target_speed ) {
         _speed += _acceleration;
-        if (_speed > _target_speed) _speed = _target_speed;
-      } else if (_speed > _target_speed) {
+        if( _speed > _target_speed ) _speed = _target_speed;
+      } else if( _speed > _target_speed ) {
         _speed -= _acceleration;
-        if (_speed < _target_speed) _speed = _target_speed;
+        if( _speed < _target_speed ) _speed = _target_speed;
       }
     }
 
     // 回転方向の設定
-    if (_speed >= 0) {
-      digitalWrite(_dirPin, HIGH);
+    if( _speed >= 0 ) {
+      digitalWrite( _dirPin, HIGH );
     } else {
-      digitalWrite(_dirPin, LOW);
+      digitalWrite( _dirPin, LOW );
     }
-    float abs_speed = abs(_speed);
+    float abs_speed = abs( _speed );
 
     // パルスの生成間隔の計算
-    uint32_t wait = static_cast<uint32_t>(
-        60000000.0 / (static_cast<float>(_pulse_count) * 2.0 * abs_speed));
+    uint32_t wait = static_cast<uint32_t>( 60000000.0 / ( static_cast<float>( _pulse_count ) * 2.0 * abs_speed ) );
     // パルスの生成
-    if (_time_count >= wait) {
+    if( _time_count >= wait ) {
       _time_count = 0;
-      digitalWrite(_pulPin, 1 - digitalRead(_pulPin));
+      digitalWrite( _pulPin, 1 - digitalRead( _pulPin ) );
     }
   }
 }
